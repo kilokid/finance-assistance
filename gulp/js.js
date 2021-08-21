dcd90171eb682b517824e38a113075ffb5dc056c
greatstudentkaze <dfyz0r@list.ru>
2021-08-21 14:37:49 +0300

    feat(gulp): compile js by webpack

--- gulp/js.js
+++ gulp/js.js
@@ -1,15 +1,33 @@
 import gulp from 'gulp';
-import babel from 'gulp-babel';
-import terser from 'gulp-terser';
-import rename from 'gulp-rename';
+import webpack from 'webpack-stream';
 import browserSync from 'browser-sync';
+
+const webpackConfig = {
+  mode: 'production',
+  entry: {
+    script: './src/js/script.js',
+  },
+  output: {
+    filename: '[name].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
 
 export const js = () =>
   gulp.src('src/js/**/*.js')
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
-    .pipe(terser())
-    .pipe(rename({extname: '.min.js'}))
+    .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('build/js'))
     .pipe(browserSync.stream());
