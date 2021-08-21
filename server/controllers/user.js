5fcb04a5e44b6d5c4f27f19d079b2fafa324fe41
greatstudentkaze <dfyz0r@list.ru>
2021-08-21 17:23:50 +0300

    feat(server): add user controller

---
+++ server/controllers/user.js
@@ -0,0 +1,51 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+
+  async refreshToken(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+}
+
+export const userController = new UserController();
--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
+import { userController } from '../controllers/user.js';
+
 const router = new Router();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refreshToken);
+router.get('/users', userController.getUsers);
 
 export { router };
