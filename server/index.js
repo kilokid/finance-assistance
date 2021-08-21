fa81967109b1fa3c0d0d1375583b2278641612d9
greatstudentkaze <dfyz0r@list.ru>
2021-08-21 17:04:30 +0300

    feat(server): connect to mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -10,7 +11,17 @@ const PORT = process.env.PORT ?? 5000;
 const app = express();
 
 const start = async () => {
+  if (!process.env.DATABASE_URL) {
+    console.log('Get database url from https://cloud.mongodb.com/v2/612104f5de540955eabc98da#clusters/connect?clusterId=Cluster0');
+    return;
+  }
+
   try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
   } catch (err) {
     console.log(err);
