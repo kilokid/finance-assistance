f0c17964cdd86cb2afe58d89bb5c6ee25586888f
greatstudentkaze <dfyz0r@list.ru>
2021-08-21 17:05:26 +0300

    feat(server): add middlewares

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ dotenv.config();
 const PORT = process.env.PORT ?? 5000;
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 const start = async () => {
   if (!process.env.DATABASE_URL) {
