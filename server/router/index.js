d71af3fac7af139bfbf0abc59e4722aefda3e519
greatstudentkaze <dfyz0r@list.ru>
2021-08-22 16:29:05 +0300

    feat(server): add authMiddleware to restrict access

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
 
+import { auth as authMiddleware } from '../middlewares/auth.js';
 import { userController } from '../controllers/user.js';
 
 const router = new Router();
@@ -16,6 +17,6 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refreshToken);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 export { router };
