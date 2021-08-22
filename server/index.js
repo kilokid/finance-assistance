85a2339f1595ebad436f1444af7a20ded99543be
greatstudentkaze <dfyz0r@list.ru>
2021-08-22 16:51:16 +0300

    feat(server): update cors options

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT ?? 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL,
+}));
 app.use('/api', router);
 app.use(error);
 
