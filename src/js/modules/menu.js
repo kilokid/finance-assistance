ea43e449cde3c0d08f6cea834cb7f74bbff76054
kilokid <arlomov007@gmail.com>
2021-09-12 20:25:26 +0300

    fix: menu.js

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,9 +1,8 @@
 export const menu = () => {
-  console.log('menu.js initialized');
-};
-const btn = document.querySelector('.menu__burger');
-const menuItem = document.querySelector('.menu__links');
+  const btn = document.querySelector('.menu__burger');
+  const menuItem = document.querySelector('.menu__links');
 
-btn.addEventListener('click', () => {
-  menuItem.classList.toggle('active');
-});
+  btn.addEventListener('click', () => {
+    menuItem.classList.toggle('active');
+  });
+};
