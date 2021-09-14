63607b1eb7a70105943d9d35659dbabaf724b853
kilokid <arlomov007@gmail.com>
2021-09-14 18:45:13 +0300

    create: open/close modal window

--- src/js/modules/login-popup.js
+++ src/js/modules/login-popup.js
@@ -1,3 +1,25 @@
 export const loginPopup = () => {
+  const buttonLogin = document.querySelector('.menu__entrance');
+  const modal = document.querySelector('.modal');
 
+  const openModal = () => {
+    modal.classList.remove('modal--hidden');
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    modal.classList.add('modal--hidden');
+    document.body.style.overflow = '';
+  };
+
+  buttonLogin.addEventListener('click', openModal);
+
+  modal.addEventListener('click', (event) => {
+    const target = event.target;
+    console.log(target);
+    if (target.classList.contains('modal__overlay') || target.classList.contains('modal__close')) {
+        closeModal();
+    }
+  });
 };
+
