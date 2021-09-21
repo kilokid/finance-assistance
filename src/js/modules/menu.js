e84fda912d63f0c238a42f3ddd280cae333375d5
kilokid <arlomov007@gmail.com>
2021-09-22 02:44:10 +0300

    fix: structure menu

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,25 +1,22 @@
-// export const menu = () => {
-//   const btn = document.querySelector('.menu__burger');
-//   const menuElem = document.querySelector('.menu__links');
-//   const menuLogin = document.querySelector('.menu__login');
+export const menu = () => {
+  const btn = document.querySelector('.menu__burger');
+  const menuElem = document.querySelector('.menu__links');
 
-//   btn.addEventListener('click', () => {
-//     menuElem.classList.toggle('menu__links--active');
-//     menuLogin.classList.toggle('menu__login--active');
-//   });
+  btn.addEventListener('click', () => {
+    menuElem.classList.toggle('menu__links--active');
+  });
 
-//   const closeMenu = () => {
-//     menuElem.classList.remove('menu__links--active');
-//     menuLogin.classList.remove('menu__login--active');
-//   };
-//   // Сделал твоим способом закрытие меню по клику вне его :)
-//   document.addEventListener('click', (event) => {
-//     const target = event.target;
+  const closeMenu = () => {
+    menuElem.classList.remove('menu__links--active');
+  };
+  // Сделал твоим способом закрытие меню по клику вне его :)
+  document.addEventListener('click', (event) => {
+    const target = event.target;
 
-//     if (target.closest('.menu')) {
-//       return;
-//     }
+    if (target.closest('.menu')) {
+      return;
+    }
 
-//     closeMenu();
-//   });
-// };
+    closeMenu();
+  });
+};
