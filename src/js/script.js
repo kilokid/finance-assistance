966f81fc8d0cc384ab27bbf64661fffaa75b3d5a
kilokid <arlomov007@gmail.com>
2021-11-13 23:35:17 +0300

    add: slider plugin

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,25 @@
 import { loginPopup } from './modules/login-popup';
 import { menu } from './modules/menu';
+import sliderCarousel from '../../plugins/sliderCarousel/sliderCarousel';
 
 loginPopup();
 menu();
+
+const carousel = new sliderCarousel({
+  main: '.reviews',
+  wrap: '.reviews__cards',
+  prev: '.reviews__prev',
+  next: '.reviews__next',
+  slidesToShow: 1,
+  infinity: true,
+
+  responsive: [{
+      breakpoint: 1020,
+      slideToShow: 1
+  },
+  {
+      breakpoint: 768,
+      slideToShow: 1
+  }]
+});
+carousel.init();
